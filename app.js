f8d1d30d30e4a67550a3bf288512c1a2d971e75a
muflihnurfaizi <nurfaizimuflih10@gmail.com>
2021-12-25 09:57:21 +0700

    backend entry poin

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Require
 const express = require("express");
 const tasks = require("./routes/tasks");
 const app = express();
@@ -7,15 +8,16 @@ const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static("./public"));
+app.use(express.static("public"));
 app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
+// Connect to db and start the server
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
